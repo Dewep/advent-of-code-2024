3a60be0ae50e7012b445055e38f46a45df3c14f9
Aurelien Maigret <github@dewep.net>
2024-12-10 10:12:38 +0100

    Day 10

---
+++ day10/day10-part2.mjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env zx
+
+import { read } from '../utils/parse.mjs'
+
+const raw = await read()
+
+const map = raw.split('\n').filter(l => l).map(l => l.split('').map(nb => +nb))
+
+let score = 0
+
+for (let y = 0; y < map.length; y++) {
+  for (let x = 0; x < map[y].length; x++) {
+    if (map[y][x] === 0) {
+      score += countTrailheads(y, x).total
+    }
+  }
+}
+
+console.log(score)
+
+function countTrailheads (y, x, find = 0, solutions = { total: 0 }) {
+  if (y >= 0 && y < map.length && x >= 0 && x < map[y].length && map[y][x] === find) {
+    if (find === 9) {
+      solutions.total += 1
+    } else {
+      countTrailheads(y - 1, x, find + 1, solutions)
+      countTrailheads(y + 1, x, find + 1, solutions)
+      countTrailheads(y, x - 1, find + 1, solutions)
+      countTrailheads(y, x + 1, find + 1, solutions)
+    }
+  }
+  return solutions
+}
